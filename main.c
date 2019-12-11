1382464bafc10e8aafc0b45c0e0971549d396b3f
butlejac <42748505+butlejac@users.noreply.github.com>
2019-12-10 22:56:14 -0500

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-#include <dirent.h>
 typedef unsigned long int UINT4;
 typedef struct {
     UINT4 i[2];
@@ -121,82 +120,82 @@ UINT4 *in;
 #define S12 12
 #define S13 17
 #define S14 22
-    FF(a, b, c, d, in[ 0], S11, 3614090360); /* 1 */
-    FF(d, a, b, c, in[ 1], S12, 3905402710); /* 2 */
-    FF(c, d, a, b, in[ 2], S13, 606105819); /* 3 */
-    FF(b, c, d, a, in[ 3], S14, 3250441966); /* 4 */
-    FF(a, b, c, d, in[ 4], S11, 4118548399); /* 5 */
-    FF(d, a, b, c, in[ 5], S12, 1200080426); /* 6 */
-    FF(c, d, a, b, in[ 6], S13, 2821735955); /* 7 */
-    FF(b, c, d, a, in[ 7], S14, 4249261313); /* 8 */
-    FF(a, b, c, d, in[ 8], S11, 1770035416); /* 9 */
-    FF(d, a, b, c, in[ 9], S12, 2336552879); /* 10 */
-    FF(c, d, a, b, in[10], S13, 4294925233); /* 11 */
-    FF(b, c, d, a, in[11], S14, 2304563134); /* 12 */
-    FF(a, b, c, d, in[12], S11, 1804603682); /* 13 */
-    FF(d, a, b, c, in[13], S12, 4254626195); /* 14 */
-    FF(c, d, a, b, in[14], S13, 2792965006); /* 15 */
-    FF(b, c, d, a, in[15], S14, 1236535329); /* 16 */
+    FF(a, b, c, d, in[ 0], S11, 3614090360);
+    FF(d, a, b, c, in[ 1], S12, 3905402710);
+    FF(c, d, a, b, in[ 2], S13, 606105819);
+    FF(b, c, d, a, in[ 3], S14, 3250441966);
+    FF(a, b, c, d, in[ 4], S11, 4118548399);
+    FF(d, a, b, c, in[ 5], S12, 1200080426);
+    FF(c, d, a, b, in[ 6], S13, 2821735955);
+    FF(b, c, d, a, in[ 7], S14, 4249261313);
+    FF(a, b, c, d, in[ 8], S11, 1770035416);
+    FF(d, a, b, c, in[ 9], S12, 2336552879);
+    FF(c, d, a, b, in[10], S13, 4294925233);
+    FF(b, c, d, a, in[11], S14, 2304563134);
+    FF(a, b, c, d, in[12], S11, 1804603682);
+    FF(d, a, b, c, in[13], S12, 4254626195);
+    FF(c, d, a, b, in[14], S13, 2792965006);
+    FF(b, c, d, a, in[15], S14, 1236535329);
 #define S21 5
 #define S22 9
 #define S23 14
 #define S24 20
-    GG(a, b, c, d, in[ 1], S21, 4129170786); /* 17 */
-    GG(d, a, b, c, in[ 6], S22, 3225465664); /* 18 */
-    GG(c, d, a, b, in[11], S23, 643717713); /* 19 */
-    GG(b, c, d, a, in[ 0], S24, 3921069994); /* 20 */
-    GG(a, b, c, d, in[ 5], S21, 3593408605); /* 21 */
-    GG(d, a, b, c, in[10], S22, 38016083); /* 22 */
-    GG(c, d, a, b, in[15], S23, 3634488961); /* 23 */
-    GG(b, c, d, a, in[ 4], S24, 3889429448); /* 24 */
-    GG(a, b, c, d, in[ 9], S21, 568446438); /* 25 */
-    GG(d, a, b, c, in[14], S22, 3275163606); /* 26 */
-    GG(c, d, a, b, in[ 3], S23, 4107603335); /* 27 */
-    GG(b, c, d, a, in[ 8], S24, 1163531501); /* 28 */
-    GG(a, b, c, d, in[13], S21, 2850285829); /* 29 */
-    GG(d, a, b, c, in[ 2], S22, 4243563512); /* 30 */
-    GG(c, d, a, b, in[ 7], S23, 1735328473); /* 31 */
-    GG(b, c, d, a, in[12], S24, 2368359562); /* 32 */
+    GG(a, b, c, d, in[ 1], S21, 4129170786);
+    GG(d, a, b, c, in[ 6], S22, 3225465664);
+    GG(c, d, a, b, in[11], S23, 643717713);
+    GG(b, c, d, a, in[ 0], S24, 3921069994);
+    GG(a, b, c, d, in[ 5], S21, 3593408605);
+    GG(d, a, b, c, in[10], S22, 38016083);
+    GG(c, d, a, b, in[15], S23, 3634488961);
+    GG(b, c, d, a, in[ 4], S24, 3889429448);
+    GG(a, b, c, d, in[ 9], S21, 568446438);
+    GG(d, a, b, c, in[14], S22, 3275163606);
+    GG(c, d, a, b, in[ 3], S23, 4107603335);
+    GG(b, c, d, a, in[ 8], S24, 1163531501);
+    GG(a, b, c, d, in[13], S21, 2850285829);
+    GG(d, a, b, c, in[ 2], S22, 4243563512);
+    GG(c, d, a, b, in[ 7], S23, 1735328473);
+    GG(b, c, d, a, in[12], S24, 2368359562);
 #define S31 4
 #define S32 11
 #define S33 16
 #define S34 23
-    HH(a, b, c, d, in[ 5], S31, 4294588738); /* 33 */
-    HH(d, a, b, c, in[ 8], S32, 2272392833); /* 34 */
-    HH(c, d, a, b, in[11], S33, 1839030562); /* 35 */
-    HH(b, c, d, a, in[14], S34, 4259657740); /* 36 */
-    HH(a, b, c, d, in[ 1], S31, 2763975236); /* 37 */
-    HH(d, a, b, c, in[ 4], S32, 1272893353); /* 38 */
-    HH(c, d, a, b, in[ 7], S33, 4139469664); /* 39 */
-    HH(b, c, d, a, in[10], S34, 3200236656); /* 40 */
-    HH(a, b, c, d, in[13], S31, 681279174); /* 41 */
-    HH(d, a, b, c, in[ 0], S32, 3936430074); /* 42 */
-    HH(c, d, a, b, in[ 3], S33, 3572445317); /* 43 */
-    HH(b, c, d, a, in[ 6], S34, 76029189); /* 44 */
-    HH(a, b, c, d, in[ 9], S31, 3654602809); /* 45 */
-    HH(d, a, b, c, in[12], S32, 3873151461); /* 46 */
-    HH(c, d, a, b, in[15], S33, 530742520); /* 47 */
-    HH(b, c, d, a, in[ 2], S34, 3299628645); /* 48 */
+    HH(a, b, c, d, in[ 5], S31, 4294588738);
+    HH(d, a, b, c, in[ 8], S32, 2272392833);
+    HH(c, d, a, b, in[11], S33, 1839030562);
+    HH(b, c, d, a, in[14], S34, 4259657740);
+    HH(a, b, c, d, in[ 1], S31, 2763975236);
+    HH(d, a, b, c, in[ 4], S32, 1272893353);
+    HH(c, d, a, b, in[ 7], S33, 4139469664);
+    HH(b, c, d, a, in[10], S34, 3200236656);
+    HH(a, b, c, d, in[13], S31, 681279174);
+    HH(d, a, b, c, in[ 0], S32, 3936430074);
+    HH(c, d, a, b, in[ 3], S33, 3572445317);
+    HH(b, c, d, a, in[ 6], S34, 76029189);
+    HH(a, b, c, d, in[ 9], S31, 3654602809);
+    HH(d, a, b, c, in[12], S32, 3873151461);
+    HH(c, d, a, b, in[15], S33, 530742520);
+    HH(b, c, d, a, in[ 2], S34, 3299628645);
 #define S41 6
 #define S42 10
 #define S43 15
 #define S44 21
-    II(a, b, c, d, in[ 0], S41, 4096336452); /* 49 */
-    II(d, a, b, c, in[ 7], S42, 1126891415); /* 50 */
-    II(c, d, a, b, in[14], S43, 2878612391); /* 51 */
-    II(b, c, d, a, in[ 5], S44, 4237533241); /* 52 */
-    II(a, b, c, d, in[12], S41, 1700485571); /* 53 */
-    II(d, a, b, c, in[ 3], S42, 2399980690); /* 54 */
-    II(c, d, a, b, in[10], S43, 4293915773); /* 55 */
-    II(b, c, d, a, in[ 1], S44, 2240044497); /* 56 */
-    II(a, b, c, d, in[ 8], S41, 1873313359); /* 57 */
-    II(d, a, b, c, in[15], S42, 4264355552); /* 58 */
-    II(c, d, a, b, in[ 6], S43, 2734768916); /* 59 */
-    II(b, c, d, a, in[13], S44, 1309151649); /* 60 */
-    II(a, b, c, d, in[ 4], S41, 4149444226); /* 61 */
-    II(d, a, b, c, in[11], S42, 3174756917); /* 62 */
-    II(c, d, a, b, in[ 2], S43, 718787259); /* 63 */
-    II(b, c, d, a, in[ 9], S44, 3951481745); /* 64 */
+    II(a, b, c, d, in[ 0], S41, 4096336452);
+    II(d, a, b, c, in[ 7], S42, 1126891415);
+    II(c, d, a, b, in[14], S43, 2878612391);
+    II(b, c, d, a, in[ 5], S44, 4237533241);
+    II(a, b, c, d, in[12], S41, 1700485571);
+    II(d, a, b, c, in[ 3], S42, 2399980690);
+    II(c, d, a, b, in[10], S43, 4293915773);
+    II(b, c, d, a, in[ 1], S44, 2240044497);
+    II(a, b, c, d, in[ 8], S41, 1873313359);
+    II(d, a, b, c, in[15], S42, 4264355552);
+    II(c, d, a, b, in[ 6], S43, 2734768916);
+    II(b, c, d, a, in[13], S44, 1309151649);
+    II(a, b, c, d, in[ 4], S41, 4149444226);
+    II(d, a, b, c, in[11], S42, 3174756917);
+    II(c, d, a, b, in[ 2], S43, 718787259);
+    II(b, c, d, a, in[ 9], S44, 3951481745);
     buf[0] += a;
     buf[1] += b;
     buf[2] += c;
@@ -217,7 +216,6 @@ MD5_CTX *mdContext;
     char temp[10];
     printf("\n");
     for (i = 0; i < 16; i++) {
-
         sprintf(&temp, "%02x", mdContext->digest[i]);
         result = concat(result, temp);
     }
@@ -241,7 +239,6 @@ char *filename;
     MD5_CTX mdContext;
     int bytes;
     unsigned char data[1024];
-
     if (inFile == NULL) {
         printf("%s can't be opened.\n", filename);
         return "";
@@ -251,7 +248,6 @@ char *filename;
         MD5Update(&mdContext, data, bytes);
     MD5Final(&mdContext);
     char* md5 = MDPrint(&mdContext);
-    printf("\nMD5 completed for %s\n", filename);
     fclose(inFile);
     return (md5);
 }
@@ -259,20 +255,12 @@ static void MDFilter() {
     MD5_CTX mdContext;
     int bytes;
     unsigned char data[16];
-
     MD5Init(&mdContext);
     while ((bytes = fread(data, 1, 16, stdin)) != 0)
         MD5Update(&mdContext, data, bytes);
     MD5Final(&mdContext);
     MDPrint(&mdContext);
     printf("\n");
-}
-void adx_store_data(const char *filepath, const char *data) {
-    FILE *fp = fopen(filepath, "w");
-    if (fp != NULL) {
-        fputs(data, fp);
-        fclose(fp);
-    }
 }
 void printFileContent(const char *filepath) {
     FILE *f;
@@ -284,40 +272,13 @@ void printFileContent(const char *filepath) {
     printf("\n");
 }
 int main(int argc, char** argv) {
-    printf("Before changing anything.");
-    adx_store_data("file1.txt", "This is file1 content");
-    struct dirent *de;
-    DIR *dr = opendir(".");
-    if (dr == NULL)
-    {
-        printf("Could not open current directory");
-        return 0;
-    }
-    while ((de = readdir(dr)) != NULL)
-        printf("%s\n", de->d_name);
     char* hash_0_0 = MDFile("file1.txt");
-    printf("file1 content: ");
-    printFileContent("file1.txt");
-    printf("Hash: %s", hash_0_0);
     char* hash_0_1 = MDFile("file2.txt");
-    printf("file2 content: ");
-    printFileContent("file2.txt");
-    printf("Hash: %s", hash_0_1);
     char* hash_1_0 = MDFile("file3.txt");
-    printf("file3 content: ");
-    printFileContent("file3.txt");
-    printf("Hash: %s", hash_1_0);
     char* hash_1_1 = MDFile("file4.txt");
-    printf("file4 content: ");
-    printFileContent("file4.txt");
-    printf("Hash: %s", hash_1_1);
     char* hash_0 = MDString(concat(hash_0_0, hash_0_1));
     char* hash_1 = MDString(concat(hash_1_0, hash_1_1));
     char* topHashBeforeChange = MDString(concat(hash_0, hash_1));
-    printf("Top Hash Before Change: %s\n\n", topHashBeforeChange);
-    printf("\nChanging file content of file1.txt.");
-    adx_store_data("file1.txt", "File1 content modified. this is new text");
-    printf("\nAfter changing anything file1.txt content.");
     char* hash_0_0_after = MDFile("file1.txt");
     printf("file1 content: ");
     printFileContent("file1.txt");
@@ -338,12 +299,5 @@ int main(int argc, char** argv) {
     char* hash_1_after = MDString(concat(hash_1_0_after, hash_1_1_after));
     char* topHashAfterChange = MDString(concat(hash_0_after, hash_1_after));
     printf("\nTop Hash After Change: %s", topHashAfterChange);
-    printf("\n------------------------------");
-    printf("\n------------------------------");
-    if (strcmp(topHashBeforeChange, topHashAfterChange) != 0) {
-        printf("\nFiles are modified");
-    } else {
-        printf("\nFiles are not modified");
-    }
     return (EXIT_SUCCESS);
 }
